6ce244206e9d3e8add984ed74a4fd63da65525cb
ronan <321393295@qq.com>
2023-09-14 15:00:54 +0800

    home-图片懒加载指令优化

---
+++ src/directives/index.js
@@ -0,0 +1,22 @@
+// 定义懒加载插件
+import { useIntersectionObserver } from '@vueuse/core'
+
+export const lazyPlugin = {
+    install(app) {
+        // 懒加载执行逻辑
+        app.directive('img-lazy', {
+            mounted(el, binding) {
+                // el；绑定指令的元素img, binding:.value 指令后面绑定的值
+                useIntersectionObserver(
+                    el, ([{ isIntersecting }]) => {
+                        // 进入视口区域
+                        if (isIntersecting) {
+                            el.src = binding
+                            stop()
+                        }
+                    },
+                )
+            }
+        })
+    }
+}
