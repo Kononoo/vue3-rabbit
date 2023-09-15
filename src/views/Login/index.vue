04bcab7c2cac123f1e058f5e9829386e280ddb0f
ronan <321393295@qq.com>
2023-09-15 21:59:59 +0800

    登录-表单校验

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,5 +1,24 @@
 <script setup>
 
+// 表单校验功能 (账户名+密码)
+import {ref, reactive} from "vue";
+// 1 准备表单对象
+const form = reactive({
+  account: '',
+  password: ''
+})
+// 2 准备规则对象
+const rules = {
+  account: [
+    { required:true, message:'用户名不能为空', trigger:'blur'}
+  ],
+  password: [
+    { required: true, message: '密码不能为空', trigger: 'blur'},
+    { min: 6, max: 14, message: '密码长度应该在6到14位', trigger: 'blur' },
+  ]
+}
+
+
 </script>
 
 <template>
@@ -23,13 +42,13 @@
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form label-position="right" label-width="60px"
+            <el-form :model="form" :rules="rules" label-position="right" label-width="60px"
                      status-icon>
-              <el-form-item  label="账户">
-                <el-input/>
+              <el-form-item  label="账户" prop="account">
+                <el-input v-model="form.account"/>
               </el-form-item>
-              <el-form-item label="密码">
-                <el-input/>
+              <el-form-item label="密码" prop="password">
+                <el-input v-model="form.password"/>
               </el-form-item>
               <el-form-item label-width="22px">
                 <el-checkbox  size="large">
