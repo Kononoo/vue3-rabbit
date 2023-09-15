c1416e9b7651a09c145a1250d12da5a07ec02cdc
ronan <321393295@qq.com>
2023-09-15 22:13:41 +0800

    登录-表单校验-自定义校验规则

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -5,7 +5,8 @@ import {ref, reactive} from "vue";
 // 1 准备表单对象
 const form = reactive({
   account: '',
-  password: ''
+  password: '',
+  agree: true
 })
 // 2 准备规则对象
 const rules = {
@@ -15,6 +16,15 @@ const rules = {
   password: [
     { required: true, message: '密码不能为空', trigger: 'blur'},
     { min: 6, max: 14, message: '密码长度应该在6到14位', trigger: 'blur' },
+  ],
+  agree: [
+    {
+      validator: (rule, value, callback) => {
+        console.log(value)
+        // 自定义校验逻辑
+        value ? callback() : callback(new Error('请勾选协议'))
+      }
+    }
   ]
 }
 
@@ -50,8 +60,8 @@ const rules = {
               <el-form-item label="密码" prop="password">
                 <el-input v-model="form.password"/>
               </el-form-item>
-              <el-form-item label-width="22px">
-                <el-checkbox  size="large">
+              <el-form-item prop="agree" label-width="22px">
+                <el-checkbox  size="large" v-model="form.agree">
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
