eb393fb18c7b7b67cfc7de1aaadb95429af98195
ronan <321393295@qq.com>
2023-09-16 14:10:32 +0800

    登录-token拦截器携带

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,6 +2,7 @@
 
 import axios from "axios";
 import {ElMessage} from "element-plus";
+import {useUserStore} from "@/stores/user";
 
 const httpInstance = axios.create({
     baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
@@ -10,6 +11,12 @@ const httpInstance = axios.create({
 
 // axios请求拦截器
 httpInstance.interceptors.request.use(config => {
+    // 1 从pinia获取token数据  2 按后端要求拼接token数据
+    const userStore = useUserStore()
+    const token = userStore.userInfo.token
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`
+    }
     return config
 }, e => Promise.reject(e))
 
