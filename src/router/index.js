3ba57bf05c7902f2ccc8b62448dbebc947a46139
ronan <321393295@qq.com>
2023-09-17 19:03:28 +0800

    结算页-路由配置&基础数据渲染

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ import Category from '@/views/Category/index.vue'
 import SubCategory from '@/views/SubCategory/index.vue'
 import Detail from '@/views/Detail/index.vue'
 import CartList from '@/views/CartList/index.vue'
+import Checkout from '@/views/Checkout/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -36,6 +37,10 @@ const router = createRouter({
         {
           path: 'cartlist',
           component: CartList,
+        },
+        {
+          path: 'checkout',
+          component: Checkout
         }
       ]
     },
